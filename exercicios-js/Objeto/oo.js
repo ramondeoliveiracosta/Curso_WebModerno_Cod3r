9b4725a9c4550af923c9a7564e74201c9d0d864e
ramondeoliveiracosta <ramondeoliveiracosta@gmail.com>
2021-11-03 14:58:25 -0300

    Aula introdutoria a orientação a objetos em JS

---
+++ exercicios-js/Objeto/oo.js
@@ -0,0 +1,24 @@
+//CÓDIGO NÃO EXECUTÁVEL!!!
+
+//Procedural
+processamento(valor1 , valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+
+}
+
+objeto.processamento()
+
+//Principior importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (Prototype)
+// 4. Polimorfismo
+
